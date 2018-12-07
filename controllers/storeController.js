f9afca0afe75be3a0741d656184cf0509e7a84e4
Dennis Jefferson <dennismjefferson@gmail.com>
2018-12-07 15:07:52 -0800

    Add Store model

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,14 +1,5 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name = 'Den';
-  if (true) {
-    throw Error('bad');
-  }
-  next();
-};
+//
 
 exports.homePage = (req, res) => {
-  console.log(req.name);
-  res.cookie('name', 'lanme', { maxAge: 1000000 });
-
   res.render('index');
 };
