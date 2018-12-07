917655e59a0af760eb3a95cd88c187a7eebc2a30
Dennis Jefferson <dennismjefferson@gmail.com>
2018-12-07 14:37:57 -0800

    Add test controller and middleware

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-// Do work here
-router.get('/', (req, res) => {
-  console.log(req.query.please);
-  const hey = { msg: 'hello!', from: 'fam' };
+const storeController = require('../controllers/storeController');
 
-  // res.send('Hey! It works!');
-  res.render('<h1>hello</h1>');
-});
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-});
+router.get('/', storeController.myMiddleware, storeController.homePage);
 
 module.exports = router;
