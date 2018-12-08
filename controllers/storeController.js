3f3d7dec38befe2944f381180795281f796adaea
Dennis Jefferson <dennismjefferson@gmail.com>
2018-12-08 01:24:38 -0800

    Add front end and api support for editing stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -32,3 +32,19 @@ exports.editStore = async (req, res) => {
 
   res.render('editStore', { title: `Edit ${store.name}`, store });
 };
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true
+  }).exec();
+
+  req.flash(
+    'success',
+    `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${
+      store.slug
+    }">View Store →</a>`
+  );
+
+  res.redirect(`/stores/${store._id}/edit`);
+};
