4ba7bbb2eeac0492c24df15cc94474ac20179223
Dennis Jefferson <dennismjefferson@gmail.com>
2018-12-07 16:03:13 -0800

    Setup add store post method and save to db

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const { catchErrors } = require('../handlers/errorHandlers');
+
 const storeController = require('../controllers/storeController');
 
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
