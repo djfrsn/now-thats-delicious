dade814b399af2f3e3d6f2ccca2ca1595b325168
Dennis Jefferson <dennismjefferson@gmail.com>
2018-12-08 13:54:44 -0800

    Return all stores when tag param undefined

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -94,9 +94,10 @@ exports.getStoreBySlug = async (req, res, next) => {
   res.render('store', { title: store.name, store });
 };
 exports.getStoresByTag = async (req, res, next) => {
-  const tagsPromise = Store.getTagsList();
   const tag = req.params.tag;
-  const storesPromise = Store.find({ tags: tag });
+  const tagQuery = tag || { $exists: true };
+  const tagsPromise = Store.getTagsList();
+  const storesPromise = Store.find({ tags: tagQuery });
 
   const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
 
