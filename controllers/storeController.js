f2c471091f3abe18685794358c9ce1211579a358
Dennis Jefferson <dennismjefferson@gmail.com>
2018-12-08 02:26:03 -0800

    Use google places API to retrieve lat lng coordinates

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -34,6 +34,7 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
+  req.body.location.type = 'Point'; // Set location data to be a point
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true, // return the new store instead of the old one
     runValidators: true
