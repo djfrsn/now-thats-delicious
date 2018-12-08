e39c4aa1b5362e30f979937146f0d7ab57d8f507
Dennis Jefferson <dennismjefferson@gmail.com>
2018-12-08 03:24:08 -0800

    Cleanup uploads and add ui and api support for uploading photos

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,6 +1,22 @@
 const mongoose = require('mongoose');
-
 const Store = mongoose.model('Store');
+
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multer = require('multer');
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: "That filetype isn't allowed!" }, false);
+    }
+  }
+};
 
 exports.homePage = (req, res) => {
   res.render('index', { title: 'Home' });
@@ -8,6 +24,24 @@ exports.homePage = (req, res) => {
 
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
+};
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  // check if there is no new file to resize
+  if (!req.file) {
+    return next(); // go to next middleware
+  }
+  const extension = req.file.mimetype.split('/')[1];
+
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  // resize now
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  // after writing photo to filesystem
+  next();
 };
 
 exports.createStore = async (req, res) => {
